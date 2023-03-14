b9951e85268f3b33333748d145a427a8e1474bfd
aurelliatirza <126539060+aurelliatirza@users.noreply.github.com>
2023-03-14 20:45:37 +0700

    Update 1. JumDeret.c

--- PRAKTIKUM 3/1. JumDeret.c
+++ PRAKTIKUM 3/1. JumDeret.c
@@ -1,14 +1,20 @@
+//Nama: Tirza Aurellia Wijaya//
+//NIM: 24060122130047//
+//Tanggal Pembuatan: Selasa, 14 Maret 2023//
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
+int main(void){
+  
+//Kamus//
     int N, i, total = 0;
     printf("Masukkan nilai N: ");
     scanf("%d", &N);
+//Algoritma//
     for(i = 1; i <= N; i++)
     {
         total += i;
     }
     printf("Jumlah total deret bilangan adalah: %d", total);
-
+return 0;
 }
