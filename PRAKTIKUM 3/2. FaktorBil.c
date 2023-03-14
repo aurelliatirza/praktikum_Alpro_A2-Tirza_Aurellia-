b97d98c8da4be84430d046a00d70fd110f65c990
aurelliatirza <126539060+aurelliatirza@users.noreply.github.com>
2023-03-14 20:46:42 +0700

    Update 2. FaktorBil.c

--- PRAKTIKUM 3/2. FaktorBil.c
+++ PRAKTIKUM 3/2. FaktorBil.c
@@ -1,11 +1,16 @@
+//Nama: Tirza Aurellia Wijaya//
+//NIM: 24060122130047//
+//Tanggal Pembuatan: Selasa, 14 Maret 2023//
+
 #include<stdio.h>
 
 int main(){
+//Kamus//
    int N,i;
 
    printf("Masukkan nilai N : ");
    scanf("%d", &N);
-
+//Algoritma//
    for(i = 1; i <= N; i++)
     {
         if(N % i == 0)
