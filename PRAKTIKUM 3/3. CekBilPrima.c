188523f8277ee416ac9faa71f9a5f13332651713
aurelliatirza <126539060+aurelliatirza@users.noreply.github.com>
2023-03-14 20:47:42 +0700

    Update 3. CekBilPrima.c

--- PRAKTIKUM 3/3. CekBilPrima.c
+++ PRAKTIKUM 3/3. CekBilPrima.c
@@ -1,16 +1,19 @@
+//Nama: Tirza Aurellia Wijaya//
+//NIM: 24060122130047//
+//Tanggal Pembuatan: Selasa, 14 Maret 2023//
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-
+int main(void){
+//Kamus//
     int N; 
     int i; 
     int faktor = 0; 
     int val;
     printf("Masukkan nilai bilangan bulat positif N = ");
     val = scanf("%d",&N);
-
+//Algoritma//
     if (N <= 0) {
         printf("N harus positif");
     } else {
